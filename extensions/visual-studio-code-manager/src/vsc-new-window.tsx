7f293846b6b7d72c962bbe06d633f8bc7127b277
ninnka <hennzr@gmail.com>
2024-09-05 16:44:38 +0800

    feat: use cursor open new window

--- extensions/visual-studio-code-manager/src/vsc-new-window.tsx
+++ extensions/visual-studio-code-manager/src/vsc-new-window.tsx
@@ -2,7 +2,7 @@ import { closeMainWindow } from "@raycast/api";
 import { exec } from "child_process";
 
 export default async function Command() {
-  exec("code -n");
+  exec("cursor -n");
   closeMainWindow();
   return null;
 }
